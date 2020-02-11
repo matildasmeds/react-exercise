24e5844cab70a6d5b053c614002b2eb69fddcfb4
Matilda <matilda.smeds@gmail.com>
2020-02-11 17:21:41 +0100

    Basic router implementation

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
 
 class NavBar extends Component {
-
   navigate() {
     console.log('Navigate');
   }
 
   render() {
     return (
-      <div>
-         <button onClick={this.navigate}>List</button> | <button onClick={this.navigate}>New</button>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">About</Link>
+              </li>
+              <li>
+                <Link to="/recipes">Recipes</Link>
+              </li>
+              <li>
+                <Link to="/recipes/new">New Recipe</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+          <Route path='/recipes/new'><RecipesNew /></Route>
+          <Route path='/recipes'><RecipesList /></Route>
+            <Route path='/'><About /></Route>
+          </Switch>
+
+        </div>
+      </Router>
     )
   }
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+function RecipesList() {
+  return <h2>All Recipes</h2>;
+}
+function RecipesNew() {
+  return <h2>New Recipe</h2>;
 }
 
 export default NavBar;
