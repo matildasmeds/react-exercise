a7841e83c8c0d70708cf2354a0edac5ff6a72a26
Matilda <matilda.smeds@gmail.com>
2020-02-12 12:02:54 +0100

    Store Recipe name & description to backend

--- src/RecipesNew.js
+++ src/RecipesNew.js
@@ -4,26 +4,41 @@ class RecipesNew extends Component {
   constructor(props) {
     super(props);
     this.state = { name: '', description: '', ingredients: [] };
+    this.saveRecipe = this.saveRecipe.bind(this);
   }
 
   async saveRecipe() {
-    const url = 'http://localhost:8000/api/recipes';
+    const url = 'http://localhost:8000/api/recipe/';
     const payload = {
       name: this.state.name,
       description: this.state.description,
       ingredients: this.state.ingredients
     };
+    console.log(payload);
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    });
   }
 
   // TODO: Make responsive if there is time
   render() {
     return <div>
       <h2>New Recipe</h2>
-      <form>
-        <span>Name: <input type='text' name='name'></input></span><br/>
-        <span>Description: <input type='text'></input></span><br/>
-        <span>TODO: Add ingredients, either + button for adding next one, or comma separated</span><br/>
-      </form>
+      <div>Name:
+        <input type='text'
+               onChange={e => this.setState({name: e.target.value})}>
+        </input>
+      </div>
+      <div>Description:
+        <input type='text'
+               onChange={e => this.setState({description: e.target.value})}>
+        </input></div>
+      <div>TODO: Add ingredients, either + button for ndaadding next one, or comma separated</div>
+      <div><button onClick={this.saveRecipe}>Submit</button></div>
     </div>;
   }
 }
