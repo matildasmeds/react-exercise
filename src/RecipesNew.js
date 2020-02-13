393c8a3d582c0f22b9d8fe78e7672043e800ce94
Matilda <matilda.smeds@gmail.com>
2020-02-13 10:29:27 +0100

    RecipesNew should use React Hooks

--- src/RecipesNew.js
+++ src/RecipesNew.js
@@ -1,66 +1,58 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Button from './Button.js';
 
-class RecipesNew extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: '', description: '', ingredients: [] };
-    this.setIngredients = this.setIngredients.bind(this);
-    this.saveRecipe = this.saveRecipe.bind(this);
+function RecipesNew() {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [ingredients, setIngredients] = useState('');
+
+  const getPayload = () => {
+    return {
+      name: name,
+      description: description,
+      ingredients: ingredients.split(',').map(ingredient => {
+        return { name: ingredient }
+      })
+    }
   }
 
-  async saveRecipe() {
+  const saveRecipe = async () => {
     const url = 'http://localhost:8000/api/recipe/';
-    const payload = {
-      name: this.state.name,
-      description: this.state.description,
-      ingredients: this.state.ingredients
-    };
-    console.log(payload);
     const response = await fetch(url, {
       method: 'POST',
       headers: {
         'Content-type': 'application/json'
       },
-      body: JSON.stringify(payload)
+      body: JSON.stringify(getPayload())
     });
+    console.log(response);
   }
 
-  setIngredients(e) {
-    const ingredients = [];
-    e.target.value.split(',').map(ingredient => {
-      ingredients.push({ name: ingredient });
-    });
-    this.setState({ingredients: ingredients});
-  }
-
-  render() {
-    return <div className="pa4 black-80">
-      <h1 className="baskerville fw1 ph3 ph0-l">New recipe</h1>
-      <div>
-        <label className="db fw4 lh-copy f6" for='name'>Name</label>
-        <input type='text'
-              name='name'
-               onChange={e => this.setState({name: e.target.value})}>
-        </input>
-      </div>
-      <div className='mt3'>
-        <label className="db fw4 lh-copy f6" for='description'>Description</label>
-        <input type='text'
-               onChange={e => this.setState({description: e.target.value})}>
-        </input>
-      </div>
-      <div className='mt3'>
-      <label className="db fw4 lh-copy f6" for='ingredients'>Ingredients (comma separated!)</label>
+  return <div className="pa4 black-80">
+    <h1 className="baskerville fw1 ph3 ph0-l">New recipe</h1>
+    <div>
+      <label className="db fw4 lh-copy f6" htmlFor='name'>Name</label>
       <input type='text'
-             onChange={this.setIngredients}>
+             name='name'
+             onChange={e => setName(e.target.value)}>
       </input>
-      </div>
-      <div className='mt3'>
-        <Button label='Submit' callBack={this.saveRecipe}/>
-      </div>
-    </div>;
-  }
+    </div>
+    <div className='mt3'>
+      <label className="db fw4 lh-copy f6" htmlFor='description'>Description</label>
+      <input type='text'
+             onChange={e => setDescription(e.target.value)}>
+      </input>
+    </div>
+    <div className='mt3'>
+    <label className="db fw4 lh-copy f6" htmlFor='ingredients'>Ingredients (comma separated!)</label>
+    <input type='text'
+           onChange={e => setIngredients(e.target.value)}>
+    </input>
+    </div>
+    <div className='mt3'>
+      <Button label='Submit' callBack={saveRecipe}/>
+    </div>
+  </div>;
 }
 
 export default RecipesNew;
