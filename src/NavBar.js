5a4e34ad5d3d5407c1f3200ba54305fe846ea465
Matilda <matilda.smeds@gmail.com>
2020-02-13 11:09:07 +0100

    Refactor RecipesList into using hooks

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from 'react-router-dom';
 import RecipesList from './RecipesList.js'
 import RecipesNew from './RecipesNew.js'
--- src/RecipesList.js
+++ src/RecipesList.js
@@ -1,39 +1,30 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Recipe from './Recipe.js';
+import axios from 'axios';
 
-class RecipesList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { recipes: [] };
-    this.fetchRecipes.bind(this)();
-  }
+function RecipesList() {
+  const [recipes, setRecipes] = useState([]);
 
-  async fetchRecipes() {
+  useEffect(() => {
     const url = 'http://localhost:8000/api/recipe';
-    const response = await fetch(url);
+    axios(url).then(result => {
+      setRecipes(result.data);
+    });
+  });
 
-    if (response.ok) {
-      let recipes = await response.json();
-      this.setState({ recipes: recipes});
-    }
-  }
+  console.log(recipes);
 
-  render() {
-    const recipes_html = this.state.recipes.map(recipe =>
-      <div key={recipe.id}>
-        <Recipe
-          name={recipe.name}
-          description={recipe.description}
-          ingredients={recipe.ingredients}
-        />
-      </div>
-      );
-    return ( <div className="pa4 black-80">
-        <h1 className="baskerville fw1 ph3 ph0-l bb b--black-10">All recipes</h1>
-        {recipes_html}
-      </div>
-    )
-  }
-}
+  return <div className="pa4 black-80">
+      <h1 className="baskerville fw1 ph3 ph0-l bb b--black-10">All recipes</h1>
+      { recipes && recipes.map(recipe =>
+        <div key={recipe.id}>
+          <Recipe
+            name={recipe.name}
+            description={recipe.description}
+            ingredients={recipe.ingredients}
+          />
+         </div>)}
+    </div>;
+};
 
 export default RecipesList;
