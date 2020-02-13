9bf040761e4a87eacc728b8edb954e2cece70524
Matilda <matilda.smeds@gmail.com>
2020-02-13 11:25:21 +0100

    Extract NavLink to function

--- src/NavBar.js
+++ src/NavBar.js
@@ -7,14 +7,21 @@ import {
 import RecipesList from './RecipesList.js'
 import RecipesNew from './RecipesNew.js'
 
+function NavLink(props) {
+  return <a className="link dim black f6 pr3 f5-ns dib mr3"
+            href={props.path} title={props.label}>
+    {props.label}
+  </a>;
+}
+
 function NavBar() {
   return (
     <Router>
       <div>
         <nav className="pa3 pa4-ns mw7 center avenir">
-          <a className="link dim black f6 pr3 f5-ns dib mr3" href="/recipes" title="Recipes">Recipes</a>
-          <a className="link dim black f6 pr3 f5-ns dib mr3" href="/recipes/new" title="New">New Recipe</a>
-        </nav>
+          <NavLink path='/recipes' label='Recipes' />
+          <NavLink path='/recipes/new' label='New Recipe' />
+       </nav>
         <section className="mw7 center avenir">
           <Switch>
             <Route path='/recipes/new'><RecipesNew /></Route>
